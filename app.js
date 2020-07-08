bca392109f6e03dcc0c0976798c6104f9469dfa1
MustafaAlvi21 <60567359+MustafaAlvi21@users.noreply.github.com>
2020-07-08 15:28:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-const port = process.env.port;
+const PORT = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log('starting server at'+ port);
 });
